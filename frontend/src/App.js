7729b8b357021aab01f4bdda1b33d729e9c2cebd
mattbarron0917 <matthew.barron@macaulay.cuny.edu>
2020-02-08 22:14:10 -0500

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { UncontrolledButtonDropdown, DropdownMenu, DropdownItem, DropdownToggle, Container, Row, Col  } from 'reactstrap';
+import Main from './components/main';
+import {Link} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  render(){
+  return(
+    <div style={{height: '300px', position: 'relative'}}>
+      <UncontrolledButtonDropdown>
+         <DropdownToggle caret>
+           Dropdown
+         </DropdownToggle>
+         <DropdownMenu>
+           <DropdownItem><Link to="/resume">Resume</Link></DropdownItem>
+           <DropdownItem><Link to="/aboutme">About me</Link></DropdownItem>
+           <DropdownItem><Link to="/projects">Projects</Link></DropdownItem>
+           <DropdownItem><Link to="/contact">Contact Info</Link></DropdownItem>
+         </DropdownMenu>
+       </UncontrolledButtonDropdown>
     </div>
+
+
+
   );
 }
-
+}
 export default App;
---
+++ frontend/src/components/landingpage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Landing extends Component {
+  render() {
+    return(
+      <div style={{width:'100%', margin: 'auto'}}>
+      </div>
+    )
+  }
+}
+export default Landing;
