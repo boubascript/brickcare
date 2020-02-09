287a1584e67491a7953af52e26b290e19e7f4f40
Boubacar <boubacar.diallo1@macaulay.cuny.edu>
2020-02-09 04:22:54 -0500

    map is working\!\!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { UncontrolledButtonDropdown, DropdownMenu, DropdownItem, DropdownToggle } from 'reactstrap';
 import Main from './components/Main.js';
 import {Link} from 'react-router-dom';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import {
   Stitch,
   AnonymousCredential,
@@ -9,7 +10,16 @@ import {
 } from "mongodb-stitch-browser-sdk";
 import './App.css';
 
+const position = [43.0845, -77.6749]
+
 export default class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      providers: []
+    }
+  }
+
 
   componentDidMount() {
     // Initialize the App Client
@@ -27,6 +37,24 @@ export default class App extends Component{
       console.log(result);
       return result;
     });
+
+    this.client.callFunction("getSocrataData", []).then(result => {
+       console.log(result)
+       let providers = []
+       result.forEach(place => {
+         providers.push(place); 
+        //  {
+        //    "position": [parseFloat(place.latitude), parseFloat(place.longitude)],
+        //    "facility_name": place.facility_name,
+        //    "street_number": place.street_number,
+        //    "street_name": place.street_name,
+        //  });
+        });
+        console.log(providers[0])
+        this.setState({
+          providers: providers
+        })
+    });
   }
 
   render(){
@@ -42,7 +70,35 @@ export default class App extends Component{
           </DropdownMenu>
         </UncontrolledButtonDropdown>
         <Main/>
+        <ProviderMap providers = {this.state.providers}/>
       </div>
     );
   }
 };
+
+class ProviderMap extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render(){
+    console.log(this.props.providers) // && this.props.providers[0].position)
+    return (
+      <Map id="map" center={position} zoom={13}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+        {this.props.providers.filter( (provider) => {
+          return provider.latitude && provider.longitude;
+        }).map( (provider, key) => 
+          <Marker key ={key} position={[provider.latitude, provider.longitude]}>
+            <Popup> {provider.facility_name} <br /> {provider.street_number + " " + provider.street_name}</Popup>
+          </Marker>
+        )
+        }
+        
+      </Map>
+    );
+  }
+}
